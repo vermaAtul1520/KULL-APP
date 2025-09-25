a0fee5766ebb6666a07767efb4ee59982906eed1
Arvind Singh Yadav <arvindsinghyadav@Arvinds-MacBook-Air.local>
2025-09-25 21:42:36 +0530

    citysearch comming soon added

--- src/screens/drawer/CitySearchScreen.tsx
+++ src/screens/drawer/CitySearchScreen.tsx
@@ -1,10 +1,29 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
+import ComingSoon from '@app/components/ComingSoon';
 
 export default function CitySearchScreen() {
   return (
-    <View>
-      <Text>CitySearchScreen</Text>
+    <View style={styles.container}>
+       <ComingSoon/>
+      {/* Add your games content here */}
     </View>
-  )
-}
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontSize: 16,
+    color: '#666',
+  },
+});
